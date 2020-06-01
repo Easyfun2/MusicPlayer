0409a7309886fc292d8ae4f11907838d53d42516
easyfun2 <caixiaowei0001@126.com>
2020-06-01 22:14:38 +0800

    done

--- src/main.js
+++ src/main.js
@@ -52,6 +52,19 @@ Vue.filter('dataFormat', function (originVal) {
   return `${y}-${m}-${d}`
 })
 
+Vue.filter('dataFormat01', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + 1 + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + 1 + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + 1 + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + 1 + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+  // return `${y}-${m}-${d}`
+})
+
 // 导入全局初始化样式
 import './assets/index.css'
 
--- src/views/07.mv.vue
+++ src/views/07.mv.vue
@@ -28,78 +28,51 @@
       </div>
       <!-- 精彩评论 -->
       <div class="comment-wrap">
-        <p class="title">精彩评论<span class="number">(666)</span></p>
+        <p class="title">精彩评论<span class="number">({{mvComment.total}})</span></p>
         <div class="comments-wrap">
-          <div class="item">
+          <div class="item" v-for="(item,index) in mvComment.hotComments"
+          :key="index">
             <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
+              <img :src="item.user.avatarUrl" alt="" />
             </div>
             <div class="content-wrap">
               <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
+                <span class="name">{{item.user.nickname}}：</span>
+                <span class="comment">{{item.content}}</span>
               </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
+              <div class="re-content" v-if="item.beReplied.length >0 "
+                >
+                <span class="name">{{item.beReplied[0].user.nickname}}：</span>
+                <span class="comment">{{item.beReplied[0].content}}</span>
               </div>
-              <div class="date">2020-02-12 17:26:11</div>
+              <div class="date">{{item.time | dataFormat01}}</div>
             </div>
           </div>
         </div>
       </div>
       <!-- 最新评论 -->
       <div class="comment-wrap">
-        <p class="title">最新评论<span class="number">(666)</span></p>
+        <!-- !!! 这里的总数 -->
+        <p class="title">最新评论<span class="number">({{mvComment.total}})</span></p>
         <div class="comments-wrap">
-          <div class="item">
+          <div class="item" v-for="(item,index) in mvComment.comments"
+          :key="index">
             <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
+              <img :src="item.user.avatarUrl" alt="" />
             </div>
             <div class="content-wrap">
               <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
+                <span class="name">{{item.user.nickname}}:</span>
+                <span class="comment">{{item.content}}</span>
               </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
+              <div class="re-content" v-if="item.beReplied.length >0 ">
+                <span class="name">{{item.beReplied[0].user.nickname}}：</span>
+                <span class="comment">{{item.beReplied[0].content}}</span>
               </div>
-              <div class="date">2020-02-12 17:26:11</div>
+              <div class="date">{{item.time | dataFormat01}}</div>
             </div>
           </div>
-          <div class="item">
-            <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
-            </div>
-            <div class="content-wrap">
-              <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
-              </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
-              </div>
-              <div class="date">2020-02-12 17:26:11</div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="icon-wrap">
-              <img src="../assets/avatar.jpg" alt="" />
-            </div>
-            <div class="content-wrap">
-              <div class="content">
-                <span class="name">爱斯基摩：</span>
-                <span class="comment">谁说的，长大了依旧可爱哈</span>
-              </div>
-              <div class="re-content">
-                <span class="name">小苹果：</span>
-                <span class="comment">还是小时候比较可爱</span>
-              </div>
-              <div class="date">2020-02-12 17:26:11</div>
-            </div>
-          </div>
+ 
         </div>
       </div>
       <!-- 分页器 -->
@@ -118,19 +91,20 @@
       <h3 class="title">相关推荐</h3>
       <div class="mvs">
         <div class="items">
-          <div class="item">
+          <div class="item" v-for="(item,index) in relevanceMvList"
+          :key="index">
             <div class="img-wrap">
-              <img src="../assets/mvCover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
+              <img :src="item.cover" alt="" />
+              <span class="iconfont icon-play" 
+              @click="toMv(item.id)"></span>
               <div class="num-wrap">
                 <div class="iconfont icon-play"></div>
-                <div class="num">9912</div>
+                <div class="num">{{item.playCount | InitPlayCount}}</div>
               </div>
-              <span class="time">02:43</span>
             </div>
             <div class="info-wrap">
-              <div class="name">HEYNA</div>
-              <div class="singer">余恩</div>
+              <div class="name">{{item.name}}</div>
+              <div class="singer">{{item.artistName}}</div>
             </div>
           </div>
         </div>
@@ -141,6 +115,7 @@
 
 <script>
 import Axios from 'axios';
+// import func from '../../vue-temp/vue-editor-bridge';
 export default {
   name: 'mv',
   data() {
@@ -152,15 +127,22 @@ export default {
       // 页容量
       limit: 10,
       mvVal:{},
-      mvInfo:{}
+      mvInfo:{},
+      commentLimit:20,
+      mvComment:{},
+      relevanceMvList:[]
     };
   },
   created(){
     this.getMvList()
     this.getMvInfo()
+    this.getMvComment()
+    this.getRelevanceMv()
   },
   methods: {
     handleCurrentChange(val) {
+      this.page = val
+      this.getMvComment()
       console.log(`当前页: ${val}`);
     },
     getMvList:function(){
@@ -191,6 +173,49 @@ export default {
           this.mvInfo = res.data.data
         }
       )
+      
+    },
+    getMvComment:function(){
+      Axios({
+        url:'https://autumnfish.cn/comment/mv',
+        methods:'get',
+        params:{
+          id:this.$route.query.id,
+          limit:this.commentLimit,
+          offset:(this.page-1)*this.commentLimit
+        }
+      }).then(
+        res=>{
+          this.mvComment = res.data
+          console.log('评论数据获取成功',res)
+          console.log('评论数据获取成功',this.mvComment)
+        }
+      )
+    },
+    getRelevanceMv(){
+      Axios({
+        url:'https://autumnfish.cn/simi/mv',
+        methods:'get',
+        params:{
+          mvid:this.$route.query.id
+        }
+      }).then(
+        res=>{
+          this.relevanceMvList = res.data.mvs
+          // console.log('相关mv info',res)
+        }
+      )
+    },
+    toMv(id){
+      this.$router.push(`/mv?id=${id}`)
+    }
+  },
+  watch:{
+    $route(){
+      this.getMvList()
+      this.getMvInfo()
+      this.getMvComment()
+      this.getRelevanceMv()
       
     }
   }
